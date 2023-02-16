30bc374662168d4034ee3ee4b05470ae6c2bda8b
akaz <82073147+akazdayo@users.noreply.github.com>
2023-02-16 20:40:55 +0900

    コードをかなり見やすくしました

--- talk7.py
+++ talk7.py
@@ -1,187 +1,135 @@
 import requests
 import tkinter
-from tkinter import messagebox
 import datetime
-kari = ''
-mode = 0
-d = ''
-list = []
-# mode = 0はo 1はx
-counting = 2.0
-c = 1
+from tkinter import messagebox
+import subprocess
 
 
-def speak_bouyomi(text='ゆっくり簡易化ツールが起動しました。ベータ7.2', volume=-1, speed=-1, tone=-1):
-    res = requests.get(
-        'http://localhost:50080/Talk',
-        params={
-            'text': text,
-            'voice': 1,
-            'volume': volume,
-            'speed': speed,
-            'tone': tone})
-    return res.status_code
+class speak():
+    def __init__(self) -> None:
+        self.mode = True
+        self.d = ''
+        self.counting = 2.0
+        self.read = ''
 
-# 処理
+    def speak_bouyomi(self, text='', volume=-1, speed=-1, tone=-1):
+        res = requests.get(
+            'http://localhost:50080/Talk',
+            params={
+                'text': text,
+                'voice': 1,
+                'volume': volume,
+                'speed': speed,
+                'tone': tone})
+        return res.status_code
 
-
-def ENTER(event):
-    global d
-    global yomi
-    yomi = EditBox.get()
-    if(yomi == "exit"):
-        exit()
-
-    elif(yomi == "time"):
-        dt_now = datetime.datetime.now()
-        times = dt_now.strftime('%Y年%m月%d日 %H時%M分%S秒')
-        speak_bouyomi("速度(200)" + times, int(vol.get()))
-        yomi = times
-
-    elif(mode == True):
-        if(yomi == ''):
-            return
-        elif(yomi == "reset"):
-            reset1()
-            return()
-        if(counting == 2.0):
+    def on_press_enter(self, _):
+        self.read = EditBox.get()
+        if (self.read == "exit"):
+            exit()
+        elif (self.read == "time"):
+            dt_now = datetime.datetime.now()
+            times = dt_now.strftime('%Y年%m月%d日 %H時%M分%S秒')
+            self.speak_bouyomi("速度(200)" + times, int(vol.get()))
+            self.read = times
+        elif (self.mode == True):
+            if (self.read == ''):
+                return
+            elif (self.read == "reset"):
+                self.reset()
+                return ()
+            if (self.counting == 2.0):
+                text_widget.configure(state='normal')
+                text_widget.insert(
+                    self.counting, '---------------\n')
+                text_widget.configure(state='disabled')
+                self.counting += 1
+            self.speak_bouyomi(self.read, int(vol.get()))
+            if (self.read != ''):
+                text_widget.configure(state='normal')
+                text_widget.insert(
+                    self.counting, self.read + '\n')
+                text_widget.configure(state='disabled')
+            self.counting += 1
             text_widget.configure(state='normal')
             text_widget.insert(
-                counting, '---------------\n')
+                self.counting, '---------------\n')
             text_widget.configure(state='disabled')
-            count()
-        speak_bouyomi(yomi, int(vol.get()))
-        if(yomi != ''):
-            text_widget.configure(state='normal')
-            text_widget.insert(
-                counting, yomi + '\n')
-            text_widget.configure(state='disabled')
-        count()
+            self.counting += 1
+        # self.mode2
+        elif (self.mode == False):
+            if (self.read == ''):
+                return
+            if (self.read == '@'):
+                text_widget.configure(state='normal')
+                text_widget.insert(
+                    self.counting, '---------------\n')
+                text_widget.configure(state='disabled')
+                self.counting += 1
+                self.speak_bouyomi(self.d, int(vol.get()))
+                self.read = ''
+                self.d = ''
+            elif (self.read == "reset"):
+                self.reset()
+                return ()
+            else:
+                self.d += str(self.read)
+                text_widget.configure(state='normal')
+                text_widget.insert(
+                    self.counting, self.read + '\n')
+                text_widget.configure(state='disabled')
+            self.counting += 1
+        EditBox.delete(0, tkinter.END)
+
+    def reset(self):
+        text_widget.configure(state='normal')
+        text_widget.delete("1.0", "end")
+        text_widget.configure(state='disabled')
         text_widget.configure(state='normal')
         text_widget.insert(
-            counting, '---------------\n')
+            '1.0', '入力履歴\n')
         text_widget.configure(state='disabled')
-        count()
-    # mode2
-    elif(mode == False):
-        if(yomi == ''):
-            return
-        if(yomi == '@'):
-            # print('実行します。')
-            text_widget.configure(state='normal')
-            text_widget.insert(
-                counting, '---------------\n')
-            text_widget.configure(state='disabled')
-            count()
-            speak_bouyomi(d, int(vol.get()))
-            yomi = ''
-            d = ''
-        elif(yomi == "reset"):
-            reset1()
-            return()
-        else:
-            d += str(yomi)
-            text_widget.configure(state='normal')
-            text_widget.insert(
-                counting, yomi + '\n')
-            text_widget.configure(state='disabled')
-        count()
-        nyuuryoku = ''
-    EditBox.delete(0, tkinter.END)
 
+    def HELP(self, _):
+        messagebox.showinfo(
+            "ヘルプ", "即出力モード\n即出力にチェックをつけると使える。\n即出力モードは文章を書いてENTERを押すとすぐにゆっくりがしゃべってくれるモード。\n\n@で出力モード\n@で出力モードは即出力を空白にすると使える\n@で出力モードは改行ができるモード。\n@を入力してENTERを押すとゆっくりがしゃべってくれる。")
 
-def reset1(event):
-    global text_widget
-    text_widget.configure(state='normal')
-    text_widget.delete("1.0", "end")
-    text_widget.configure(state='disabled')
-    reset2()
+    def change(self):
+        if bln.get() == True:
+            self.mode = bln.get()
+        if bln.get() == False:
+            self.mode = bln.get()
 
+    def UNDO(self, _):
+        EditBox.delete(0, tkinter.END)
+        EditBox.insert(tkinter.END, self.read)
 
-def reset2():
-    text_widget.configure(state='normal')
-    text_widget.insert(
-        '1.0', '入力履歴\n')
-    text_widget.configure(state='disabled')
-
-
-def HELP(event):
-    messagebox.showinfo(
-        "ヘルプ", "即出力モード\n即出力にチェックをつけると使える。\n即出力モードは文章を書いてENTERを押すとすぐにゆっくりがしゃべってくれるモード。\n\n@で出力モード\n@で出力モードは即出力を空白にすると使える\n@で出力モードは改行ができるモード。\n@を入力してENTERを押すとゆっくりがしゃべってくれる。")
-
-
-def count():
-    global counting
-    global c
-    counting += 1
-    c += 1
-
-
-def change1():
-    global mode
-    if bln.get() == True:
-        mode = bln.get()
-    if bln.get() == False:
-        mode = bln.get()
-
-
-def UNDO(event):
-    EditBox.delete(0, tkinter.END)
-    EditBox.insert(tkinter.END, yomi)
-
-
-def info(event):
-    messagebox.showinfo(
-        "INFORMATION", "INFORMATION\nこのプログラムは「棒読みちゃん」をベースに作られています。\nゆっくり簡易化ツール\n作者 あかず")
-
-
-def ENTERKEY(event):
-    ENTER()
+    def info(self, _):
+        messagebox.showinfo(
+            "INFORMATION", "INFORMATION\nこのプログラムは「棒読みちゃん」をベースに作られています。\nゆっくり簡易化ツール\n作者 あかず\n")
 
 
 # セットアップ
+Speak = speak()
+
+
 root = tkinter.Tk()
 root.geometry("400x500")
-#root['bg'] = 'blue'
 Static1 = tkinter.Label(text='ゆっくり簡易化ツール')
 Static1.pack()
 bln = tkinter.BooleanVar()
 bln.set(True)
-mode = bln.get()
+
 # GUI
 EditBox = tkinter.Entry(width=50)
 EditBox.insert(tkinter.END, "ここにしゃべらせたい言葉を入力!")
 EditBox.place(x=5, y=25)
 
+outCheckBox = tkinter.Checkbutton(
+    root, variable=bln, text='即出力', onvalue=True, offvalue=False, command=Speak.change)
+outCheckBox.place(x=250, y=60)
 
-soku = tkinter.Checkbutton(
-    root, variable=bln, text='即出力', onvalue=True, offvalue=False, command=change1)
-soku.place(x=250, y=60)
-
-
-Button = tkinter.Button(text=u'ENTER', width=10)
-Button.bind("<Button-1>", ENTER)
-Button.place(x=5, y=60)
-
-# エンターキー
-root.bind('<Return>', ENTER)
-
-
-Button2 = tkinter.Button(text=u'HELP', width=10)
-Button2.bind("<Button-1>", HELP)
-Button2.place(x=315, y=2)
-
-Button3 = tkinter.Button(text=u'INFO', width=10)
-Button3.bind("<Button-1>", info)
-Button3.place(x=315, y=30)
-
-Button3 = tkinter.Button(text=u'CLEAR', width=10)
-Button3.bind("<Button-1>", reset1)
-Button3.place(x=315, y=59)
-
-Button4 = tkinter.Button(text=u'UNDO', width=10)
-Button4.bind("<Button-1>", UNDO)
-Button4.place(x=90, y=60)
+Speak.mode = bln.get()
 
 vol = tkinter.DoubleVar()
 vol.set(100)
@@ -194,15 +142,40 @@ sc = tkinter.Scale(
     command=lambda e: print('volume:%4d' % vol.get()))
 sc.place(x=0, y=88)
 
-speak_bouyomi()
-root.title(u"ゆっくり簡易化ツール")
-
 text_widget = tkinter.Text(root)
 text_widget.place(x=0, y=130, height=370, width=400)
 text_widget.configure(state='normal')
 text_widget.insert(
     '1.0', '入力履歴\n')
 text_widget.configure(state='disabled')
+
+Button = tkinter.Button(text=u'ENTER', width=10)
+Button.bind("<Button-1>", Speak.on_press_enter)
+Button.place(x=5, y=60)
+
+# エンターキー
+root.bind('<Return>', Speak.on_press_enter)
+
+
+Button2 = tkinter.Button(text=u'HELP', width=10)
+Button2.bind("<Button-1>", Speak.HELP)
+Button2.place(x=315, y=2)
+
+Button3 = tkinter.Button(text=u'INFO', width=10)
+Button3.bind("<Button-1>", Speak.info)
+Button3.place(x=315, y=30)
+
+Button3 = tkinter.Button(text=u'CLEAR', width=10)
+Button3.bind("<Button-1>", Speak.reset)
+Button3.place(x=315, y=59)
+
+Button4 = tkinter.Button(text=u'UNDO', width=10)
+Button4.bind("<Button-1>", Speak.UNDO)
+Button4.place(x=90, y=60)
+
+
+Speak.speak_bouyomi(text="ゆっくり簡易化ツールが起動しました。ベータ7.5")
+root.title(u"ゆっくり簡易化ツール")
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
